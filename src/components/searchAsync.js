0024cc7300de7ed31cd72a48497b59e3ffaadddb
Alejandro Emilio Matute Rojas <amatute@baufest.com>
2020-03-12 16:04:42 -0300

    loading

--- src/components/searchAsync.js
+++ src/components/searchAsync.js
@@ -6,10 +6,13 @@ export default () => {
 const [search, setSearch] = useState('');
 const [query, setQuery] = useState('');
 const [results, setResults] = useState([]);
+const [loading, setLoading] = useState(false);
 
 useEffect( () => {
+    document.title = 'Busqueda Async'
     async function fetchData() {
         try {
+            setLoading(true);
             const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=WH4gqMoUpIqyJBB50NeJUn7e42sT4a0V&q=${query}&limit=10&offset=0&rating=G&lang=en`);
             const json = await response.json();
             setResults(
@@ -18,7 +21,9 @@ useEffect( () => {
                 })
             );
         } 
-        catch (error) {}  
+         finally {
+            setLoading(false);
+        }  
     }
     if(query !== '') {
         fetchData();
@@ -52,7 +57,7 @@ useEffect( () => {
             </form>
             <div className="panel panel-success">
                 <div className="panel-body">
-                    {results.map(item => (
+                    {loading ? <h1>Cargando...</h1> :results.map(item => (
                       <video autoPlay loop key={item} src={item} />
                       ))}
                 </div>
