cf47e46882bc845d4fd6baa938b9904ea7c946d1
Alejandro Emilio Matute Rojas <amatute@baufest.com>
2020-03-12 14:53:36 -0300

    se dejo solo el componente del buscador

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Search from './components/searchAsync'
-import App from './App';
 import './index.css';
 
 const Router = () => {
     return(
     <div>
-        <App/>
         <Search/>
     </div>
     )
